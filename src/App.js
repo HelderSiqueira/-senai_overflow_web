a592b710e21e17369fb4f81884323ed804a323a9
HelderSiqueira <helder.erik.he@gmail.com>
2021-08-16 11:26:42 -0300

    Versão dia 16-08-21

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-function Page1(){
-  return <h1>Esta é a pagina 1</h1>;
-}
+import { GlobalStyles } from "./GlobalStyles";
+import Home from "./pages/Home";
 
-function Page2(){
-  return <h1>Esta é a pagina 2</h1>;
-}
-
-function App() {
-
-  let currentPage = 2;
-
-  return (
-    <div className="App">
-      {currentPage === 1 ? <Page1/> : <Page2/>}
-    </div>);
-}
-
-export default App;
+  function App(){
+    return(
+      <>
+        <GlobalStyles/>
+        <Home/>
+      </>
+    );
+  }
+  
+  export default App;
